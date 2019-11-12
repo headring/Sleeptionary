f36341c7e51be4c83be62b265854fb0bb308536b
Hyeok Min Kwon <khmzxc@naver.com>
2019-11-12 11:08:57 +0900

    주석 추가

--- sound.py
+++ sound.py
@@ -1,15 +1,19 @@
-import spidev
+# 들어오는 소리를 받기
+
+import spidev, time
 
 spi = spidev.SpiDev()
 spi.open(0,0)
 
+
 def analog_read(channel):
-    r=spi.xfer2([1, (8+channel)<<4,0])
-    adc_out = ((r[1]&3)<<8) + r[2]
+    r = spi.xfer2([1, (8+channel)<<4,0])
+    adc_out = ((r[1] & 3) << 8) + r[2]
     return adc_out
 
+
 while True:
-    reading=analog_read()
+    reading = analog_read()
     print(reading)
     time.sleep(1)
 
--- wavsound.py
+++ wavsound.py
@@ -1,3 +1,5 @@
+# wav to list
+
 from scipy.io import wavfile
 
 # 1초에 fs번 샘플링
