d3445e6ecfdeed9bddcc079cd852b2b3428d7c1c
Hyeok Min Kwon <khmzxc@naver.com>
2019-11-28 06:44:19 +0900

    Test gathering

--- Data_gathering/main.py
+++ Data_gathering/main.py
@@ -1,4 +1,5 @@
 import sqlite3
+import time
 import Adafruit_DHT
 
 # Connect database
@@ -14,17 +15,20 @@ except sqlite3.OperationalError:
 sensor = Adafruit_DHT.DHT11
 pin = 4
 
-humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+while 1:
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-if humidity is not None and temperature is not None:
-    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-else:
-    print('Failed to get reading. Try again!')
+    if humidity is not None and temperature is not None:
+        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
 
-li = [humidity, temperature]
-sq = '''INSERT INTO tmhd(TM, HD) VALUES(?,?)'''
-value = tuple(li)
-rm = c.execute(sq, value)
-conn.commit()
+    li = [humidity, temperature]
+    sq = '''INSERT INTO tmhd(TM, HD) VALUES(?,?)'''
+    value = tuple(li)
+    rm = c.execute(sq, value)
+    conn.commit()
+
+    time.sleep(300)
 
 conn.close()
