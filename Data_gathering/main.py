4fdfe36546aa8bb5b6800013a02c7d01a4287e04
Hyeok Min Kwon <khmzxc@naver.com>
2019-11-27 18:43:37 +0900

    Edit main.py

--- Data_gathering/main.py
+++ Data_gathering/main.py
@@ -1,12 +1,25 @@
 import sqlite3
+import Adafruit_DHT
 
+# Connect database
 db_path = ""
 try:
-    conn = sqlite3.connet(db_path)
+    conn = sqlite3.connect(db_path)
     c = conn.cursor()
-    c.execute("쿼리문")
-except:
-    print("경로 상에 DB 없음")
+    # c.execute("쿼리문")
+except sqlite3.OperationalError:
+    print("There is no database in path.")
+
+# Initialize DHT sensor
+sensor = Adafruit_DHT.DHT11
+pin = 4
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+if humidity is not None and temperature is not None:
+    print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+else:
+    print('Failed to get reading. Try again!')
 
 li = []
 sq = '''INSERT INTO table_name VALUES(?,?,?)'''
