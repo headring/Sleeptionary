cc91b09b43c79ca921822794fc0be58a7521e126
emscb <khmzxc@naver.com>
2019-12-02 13:53:36 +0900

    All commit

---
+++ Video/circuitpython-master/circuitpython-master/ports/nrf/mpconfigport.mk
@@ -0,0 +1,48 @@
+# Define an equivalent for MICROPY_LONGINT_IMPL, to pass to $(MPY-TOOL) in py/mkrules.mk
+# $(MPY-TOOL) needs to know what kind of longint to use (if any) to freeze long integers.
+# This should correspond to the MICROPY_LONGINT_IMPL definition in mpconfigport.h.
+MPY_TOOL_LONGINT_IMPL = -mlongint-impl=mpz
+
+INTERNAL_LIBM = 1
+
+USB_SERIAL_NUMBER_LENGTH = 16
+
+# All nRF ports have longints.
+LONGINT_IMPL = MPZ
+
+# Audio via PWM
+ifndef CIRCUITPY_AUDIOCORE
+CIRCUITPY_AUDIOCORE = 1
+endif
+
+CIRCUITPY_AUDIOIO = 0
+
+# The ifndef's allow overriding in mpconfigboard.mk.
+
+ifndef CIRCUITPY_AUDIOMIXER
+CIRCUITPY_AUDIOMIXER = 1
+endif
+
+ifndef CIRCUITPY_AUDIOPWMIO
+CIRCUITPY_AUDIOPWMIO = 1
+endif
+
+ifndef CIRCUITPY_AUDIOBUSIO
+CIRCUITPY_AUDIOBUSIO = 1
+endif
+
+# No I2CSlave implementation
+CIRCUITPY_I2CSLAVE = 0
+
+# enable RTC
+ifndef CIRCUITPY_RTC
+CIRCUITPY_RTC = 1
+endif
+
+# frequencyio not yet implemented
+CIRCUITPY_FREQUENCYIO = 0
+
+# CircuitPython doesn't yet support NFC so force the NFC antenna pins to be GPIO.
+# See https://github.com/adafruit/circuitpython/issues/1300
+# Defined here because system_nrf52840.c doesn't #include any of our own include files.
+CFLAGS += -DCONFIG_NFCT_PINS_AS_GPIOS
---
+++ Video/circuitpython-master/circuitpython-master/supervisor/shared/rgb_led_colors.h
@@ -0,0 +1,41 @@
+
+#define COLOR(r, g, b) (((r) << 16) | ((g) << 8) | (b))
+// For brightness == 255 (full). This will be adjusted downward for various different RGB indicators,
+// which vary in brightness.
+#define INTENSITY (0x30)
+
+#define BLACK   COLOR(0, 0, 0)
+#define GREEN   COLOR(0, INTENSITY, 0)
+#define BLUE    COLOR(0, 0, INTENSITY)
+#define CYAN    COLOR(0, INTENSITY, INTENSITY)
+#define RED     COLOR(INTENSITY, 0, 0)
+#define ORANGE  COLOR(INTENSITY, INTENSITY*2/3, 0)
+#define YELLOW  COLOR(INTENSITY, INTENSITY, 0)
+#define PURPLE  COLOR(INTENSITY, 0, INTENSITY)
+#define WHITE   COLOR(INTENSITY, INTENSITY, INTENSITY)
+
+#define BOOT_RUNNING BLUE
+#define MAIN_RUNNING GREEN
+#define SAFE_MODE    YELLOW
+#define ALL_DONE     GREEN
+#define REPL_RUNNING WHITE
+
+#define ACTIVE_WRITE 0x200000
+
+#define ALL_GOOD_CYCLE_MS 2000u
+
+#define LINE_NUMBER_TOGGLE_LENGTH 300u
+#define EXCEPTION_TYPE_LENGTH_MS 1000u
+
+#define THOUSANDS WHITE
+#define HUNDREDS  BLUE
+#define TENS      YELLOW
+#define ONES      CYAN
+
+#define INDENTATION_ERROR GREEN
+#define SYNTAX_ERROR      CYAN
+#define NAME_ERROR        WHITE
+#define OS_ERROR          ORANGE
+#define VALUE_ERROR       PURPLE
+#define MPY_ERROR         BLUE
+#define OTHER_ERROR       YELLOW
