3908465afedd3939d8a10cb524563804acef2ce4
Hyeok Min Kwon <khmzxc@naver.com>
2019-12-03 20:35:19 +0900

    Edit switch.py

--- Data_gathering/switch.py
+++ Data_gathering/switch.py
@@ -3,20 +3,21 @@ from time import sleep
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(22, GPIO.IN)
-GPIO.setup(23, GPIO.IN)
-GPIO.setup(24, GPIO.IN)
+GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 try:
-	while True :
-		if GPIO.input(22) != 0:
-			print "good!"
-		if GPIO.input(23) != 0:
-			print "normal"
-		if GPIO.input(24) != 0:
-			print "bad"
+	while True:
+		if GPIO.input(22) == GPIO.HIGH:
+			print(22)
+#	 	if GPIO.input(22) != 0:
+# 			print("good!")
+#		if GPIO.input(23) != 0:
+#			print("normal")
+#		if GPIO.input(24) != 0:
+#			print("bad")
 		sleep(3)
-
 
 except KeyboardInterrupt:
 	GPIO.cleanup()
