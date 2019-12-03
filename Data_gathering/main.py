38870e1e16c973894ecbf85d730b5884554998c8
Hyeok Min Kwon <khmzxc@naver.com>
2019-12-03 17:30:27 +0900

    Modulation main.py

--- Data_gathering/main.py
+++ Data_gathering/main.py
@@ -2,6 +2,34 @@ import sqlite3
 import time
 import Adafruit_DHT
 
+
+def db_insert(li, qr):
+    value = tuple(li)
+    rm = c.execute(qr, value)
+    conn.commit()
+
+
+def tmhd():
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+    if humidity is not None and temperature is not None:
+        print("Temp={0:0.1f}'C  Humidity={1:0.1f}%".format(temperature, humidity))
+    else:
+        print('Failed to get reading. Try again!')
+
+    query = '''INSERT INTO tmhd(TM, HD) VALUES(?,?)'''
+
+    return [[temperature, humidity], query]
+
+
+def lux():
+    # 조도 측정 코드
+    lx = 0 # 조도값
+    query = '''INSERT INTO lux(LX) VALUES(?)'''
+
+    return [lx, query]
+
+
 # Connect database
 db_path = "./test"
 try:
@@ -15,29 +43,23 @@ except sqlite3.OperationalError:
 sensor = Adafruit_DHT.DHT11
 pin = 4
 
+# Time iterator
+T = 0
+
 while 1:
-    # 온습도 저장
-    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+    # 5분 간격
+    if T % 300 == 0:
+        # 온습도 저장
+        tmhd_vq = tmhd()
+        db_insert(tmhd_vq[0], tmhd_vq[1])
 
-    if humidity is not None and temperature is not None:
-        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-    else:
-        print('Failed to get reading. Try again!')
+        # 조도 저장
+        lx_vq = lux()
+        db_insert(lx_vq[0], lx_vq[1])
+    # if T % 100 == 0:
+    #     # 최빈 온도값 저장
 
-    li = [temperature, humidity]
-    sq = '''INSERT INTO tmhd(TM, HD) VALUES(?,?)'''
-    value = tuple(li)
-    rm = c.execute(sq, value)
-    conn.commit()
-
-#    # 조도 저장
-#    # 조도 측정 코드
-#    lx = 0 # 조도값
-#    sq = '''INSERT INTO lux(LX) VALUES(?)'''
-#    value = tuple(lx)
-#    rm = c.execute(sq, value)
-#    conn.commit()
-
-    time.sleep(300)
+    T += 1
+    time.sleep(1)
 
 conn.close()
