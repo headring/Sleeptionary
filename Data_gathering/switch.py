15985c127b7a73d6f9e2ff7d2ad3d5de1994f235
emscb <emscb@github.com>
2019-12-03 11:15:59 +0900

    Edit switch.py

--- Data_gathering/switch.py
+++ Data_gathering/switch.py
@@ -1,27 +1,21 @@
 import RPi.GPIO as GPIO
- import time
+import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(22 , GPIO.IN)
-GPIO.setup(23 , GPIO.IN)
-GPIO.setup(24 , GPIO.IN)
-
-#print “Press the button”
+GPIO.setup(22, GPIO.IN)
+GPIO.setup(23, GPIO.IN)
+GPIO.setup(24, GPIO.IN)
 
 try:
-
-while True :
-	if GPIO.input(22)==0:
-		print ”good!”
-	if GPIO.input(23)==0:
-		print ”normal”
-	if GPIO.input(24)==0:
-		print ”bad”
-
-time.sleep(1)
-
-#print “Press the button (CTRL-C to exit)”
+	while True :
+		if GPIO.input(22) == 0:
+			print "good!"
+		if GPIO.input(23) == 0:
+			print "normal"
+		if GPIO.input(24) == 0:
+			print "bad"
+		time.sleep(3)
 
 except KeyboardInterrupt:
- GPIO.cleanup()
+	GPIO.cleanup()
