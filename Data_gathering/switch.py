0a4e3c149a206fbb3ca1c42efc656e9170f58577
Hyeok Min Kwon <khmzxc@naver.com>
2019-12-04 17:44:45 +0900

    Change file name

--- Data_gathering/switch.py
+++ Data_gathering/switch.py
@@ -1,27 +1,23 @@
 import RPi.GPIO as GPIO
-from time import sleep
+import time
+ 
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+ 
+request_count = int(input("Enter Button Press Count -> "))
+button_count = 0
+ 
+while True:
+    button_input = GPIO.input(15)
+    if not button_input:
+        button_count += 1
+        print("Button Pressed -> %d" % button_count)
+        if button_count >= request_count:
+              break
+        time.sleep(1)
 
-GPIO.setmode(GPIO.BCM)
+GPIO.cleanup()
+print("Button Press Ended")
 
-GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-try:
-	while True:
-		if GPIO.input(22) == GPIO.HIGH:
-			print(22)
-		if GPIO.input(23) == GPIO.HIGH:
-			print(23)
-		if GPIO.input(24) == GPIO.HIGH:
-			print(24)
-#	 	if GPIO.input(22) != 0:
-# 			print("good!")
-#		if GPIO.input(23) != 0:
-#			print("normal")
-#		if GPIO.input(24) != 0:
-#			print("bad")
-		sleep(3)
-
-except KeyboardInterrupt:
-	GPIO.cleanup()
+# 15, 18, 16(pin #)
