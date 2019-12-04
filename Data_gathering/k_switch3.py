b8d91f08fb467fbd8213d7230ee78b26e06b70c5
Hyeok Min Kwon <khmzxc@naver.com>
2019-12-04 17:41:47 +0900

    Fix lint in k_switch3.py

--- Data_gathering/k_switch3.py
+++ Data_gathering/k_switch3.py
@@ -1,26 +1,23 @@
 import RPi.GPIO as GPIO
 import time
  
-GPIO.setmode( GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)
 GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_UP)
  
-request_count = int(raw_input("Enter Button Press Count ->"))
+request_count = int(input("Enter Button Press Count -> "))
 button_count = 0
  
-while True :
-           button_input = GPIO.input(15)
- 
-           if   button_input == False :
-               button_count = button_count + 1
-               print("Button Pressed ->%d"  %(button_count ) )
-               time.sleep(1)
- 
-               if  button_count >= request_count :
-                      break
+while True:
+    button_input = GPIO.input(15)
+    if not button_input:
+        button_count += 1
+        print("Button Pressed -> %d" % button_count)
+        time.sleep(1)
+        if button_count >= request_count:
+              break
  
 GPIO.cleanup()
-print ("Button Press Ended")
+print("Button Press Ended")
 
 
-#15, 16, 18(pin #)
-
+# 15, 18, 16(pin #)
