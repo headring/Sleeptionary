0fbae3d569135801270d2493f6227d715598bc9b
Hyeok Min Kwon <khmzxc@naver.com>
2019-12-04 13:11:54 +0900

    Edit switch2.py

--- Data_gathering/switch2.py
+++ Data_gathering/switch2.py
@@ -4,27 +4,16 @@ from time import sleep
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(23, GPIO.IN)
-GPIO.setup(24, GPIO.IN)
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-try:
-	while True:
-		if GPIO.input(22) != 0:
-			print(22)
-			sleep(3)
-		if GPIO.input(23) != 0:
-			print(23)
-			sleep(3)
-		if GPIO.input(24) != 0:
-			print(24)
-			sleep(3)
-#	 	if GPIO.input(22) != 0:
-# 			print("good!")
-#		if GPIO.input(23) != 0:
-#			print("normal")
-#		if GPIO.input(24) != 0:
-#			print("bad")
-#		sleep(3)
-
-except KeyboardInterrupt:
-	GPIO.cleanup()
+while True:
+	if GPIO.input(22) != 0:
+		print(22)
+		sleep(3)
+	if GPIO.input(23) != 0:
+		print(23)
+		sleep(3)
+	if GPIO.input(24) != 0:
+		print(24)
+		sleep(3)
