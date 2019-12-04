bd4b0ee30c983c1421212b83a9f3a4550dff14fd
janginae <57429556+janginae@users.noreply.github.com>
2019-12-04 12:32:55 +0900

    Create switch2.py

---
+++ Data_gathering/switch2.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(22, GPIO.IN)
+GPIO.setup(23, GPIO.IN)
+GPIO.setup(24, GPIO.IN)
+
+try:
+	while True:
+		if GPIO.input(22) == 0:
+			print(22)
+			sleep(3)
+		if GPIO.input(23) == 0:
+			print(23)
+			sleep(3)
+		if GPIO.input(24) == 0:
+			print(24)
+			sleep(3)
+#	 	if GPIO.input(22) != 0:
+# 			print("good!")
+#		if GPIO.input(23) != 0:
+#			print("normal")
+#		if GPIO.input(24) != 0:
+#			print("bad")
+#		sleep(3)
+
+	except KeyboardInterrupt:
+		GPIO.cleanup()
