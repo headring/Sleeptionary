8e9a3a593954be2577e703a9c033dc3dc0b89bc3
headring <48895268+headring@users.noreply.github.com>
2019-12-05 17:12:34 +0900

    use_this

---
+++ Light/final_turbo.py
@@ -0,0 +1,29 @@
+import RPi.GPIO as GPIO
+import time
+
+pin = 18  # PWM pin num 18
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin, GPIO.OUT)
+p = GPIO.PWM(pin, 50)
+p.start(0)
+cnt = 0
+
+file = open(light_info.txt, mode='r')
+light_thresh = 200
+
+p.ChangeDutyCycle(0)
+
+try:
+    for l in file.readlines():
+        if int(l) <= light_thresh:
+            p.ChangeDutyCycle(14)
+            time.sleep(1)
+            p.ChangeDutyCycle(1)
+            time.sleep(60)
+        else:
+            time.sleep(60)
+except KeyboardInterrupt:
+    p.stop()
+GPIO.cleanup()
+
