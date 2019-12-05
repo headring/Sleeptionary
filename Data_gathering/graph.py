4137f62636e5fc7bf598206a9a726f43594d0c53
Hyeok Min Kwon <khmzxc@naver.com>
2019-12-05 17:38:07 +0900

    Update graph

--- Data_gathering/graph.py
+++ Data_gathering/graph.py
@@ -1,23 +1,20 @@
 from matplotlib import pyplot as plt
 from pylab import *
+import time
 
 x_data = range(0, 7)
 y_data = [5, 1, 2, 7, 8, 4, 5]
 perf = 10
 
-# plot(x_data, y_data)
+wday = {0:'월', 1:'화', 2:'수', 3:'목', 4:'금', 5:'토', 6:'일'}
+t = time.localtime()
+starttime = '%04d-%02d-%02d %02d:%02d:%02d' % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
 
 fig = plt.bar(x_data, y_data)
-plt.savefig("ggg.png")
-
-# plt.title("Test")
-# bar(x_data, y_data)
+plt.title("Last 7 days (h)")
+plt.xticks(x_data, ["Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"])
+plt.yticks([5])
 # yticks(sort(y_data))
-# plt.yticks([5])
-# plt.xticks(x_data, ["Mon", "Tue", "Wed", "Thr", "Fri", "Sat", "Sun"])
-
-# plt.show()
-
-# fig = plt.gcf()
-# fig.savefig("image.png")
-# close()
+plt.savefig("../Web/images/overview.png")
+plt.show()
+plt.close()
