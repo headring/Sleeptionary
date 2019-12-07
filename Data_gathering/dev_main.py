3b555b3f8024ee12f71f6d37d10b090d1c758ab0
Hyeok Min Kwon <khmzxc@naver.com>
2019-12-07 17:14:04 +0900

    Add graph

--- Data_gathering/dev_main.py
+++ Data_gathering/dev_main.py
@@ -1,8 +1,8 @@
 import sqlite3
 import time
 import Adafruit_DHT
-import matplotlib as mpl
-import pylab as plb
+from matplotlib import pyplot as plt
+from pylab import *
 from Adafruit_AMG88xx import Adafruit_AMG88xx
 import RPi.GPIO as GPIO
 
@@ -140,7 +140,37 @@ db_insert([date, starttime, endtime, avg_TM_HD[0], avg_TM_HD[1],
            avg_LX[0], tag], '''INSERT INTO Sleeptionary VALUES(?,?,?,?,?,?,?)''')
 
 # TODO 그래프 그리기
+y_data = []
+wday = {0: 'Mon', 1: 'Tue', 2: 'Wed', 3: 'Thr', 4: 'Fri', 5: 'Sat', 6: 'Sun'}
+xtick = []
 
+sleep_times = []
+c2 = c.execute('''SELECT Date, (julianday(end)-julianday(start))*24 FROM Sleeptionary ORDER BY Date DESC LIMIT 7''')
+for s in c2:
+    sleep_times.append(list(s))
+
+sleep_times_index = len(sleep_times) - 1
+for i in range(7):
+    # x축 라벨 만들기 (최근 7일의 날짜와 요일)
+    t = time.localtime(time.time() - 86400 * (7 - i))
+    xtick.append("%s\n%s" % ('%02d/%02d' % (t.tm_mon, t.tm_mday), wday[t.tm_wday]))
+
+    # y축에 들어갈 수면 시간
+    if not sleep_times_index < 0:
+        if sleep_times[sleep_times_index][0] == "%04d-%02d-%02d" % (t.tm_year, t.tm_mon, t.tm_mday):
+            y_data.append(float(sleep_times[sleep_times_index][1]))
+            sleep_times_index -= 1
+        else:
+            y_data.append(0)
+
+plt.bar(range(0, 7), y_data)
+plt.title("Last 7 days (h)")
+plt.xticks(range(0, 7), xtick)
+plt.yticks([5])
+# yticks(sort(y_data))
+plt.savefig("../Web/images/overview.png")
+plt.show()
+plt.close()
 
 conn.close()
 GPIO.cleanup()
